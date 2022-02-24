2d34ae3e029aedfc491eb3ad1ab22278b91d6a51
HumoFX <humo_otahanov@icloud.com>
2022-02-24 15:57:34 +0500

    notification schema changed

--- app/schemas/notification.py
+++ app/schemas/notification.py
@@ -3,18 +3,10 @@ from pydantic import BaseModel
 
 
 class Notification(BaseModel):
+    title: Optional[str]
     body: str
-    payload: Optional[dict] = None
-    newsModel: Optional[dict] = None
     fcmToken: Optional[str]
     applicationId: Optional[str]
-    id: Optional[int]
-    topic: Optional[str]
     actionType: Optional[str]
-    bannerType: Optional[str]
-    language: Optional[str]
     customerId: Optional[int]
-    url: Optional[str]
-    position: Optional[int]
-    createdDateTime: Optional[str]
-    udid: Optional[str]
+    uuid: Optional[str]
